512fbdab07219ed16c90724c83098d09761de9ff
SKA_Petersburg <144513307+kosyakarseniy20080@users.noreply.github.com>
2026-05-06 22:12:04 +1000

    Update zenit.py

---
+++ zenit.py
@@ -0,0 +1,14 @@
+def caesar_cipher(text, shift):
+    result = ""
+    for char in text:
+        if char.isalpha():
+            start = ord('A') if char.isupper() else ord('a')
+            new_char = chr((ord(char) - start + shift) % 26 + start)
+            result += new_char
+        else:
+            result += char
+    return result
+
+msg = "Hello, World!"
+encrypted = caesar_cipher(msg, 3)
+print(encrypted)
